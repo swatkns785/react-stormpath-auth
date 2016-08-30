28be3d904359c7182221272f949b4e002d79b5c0
Steven Watkins <swatkins@purch.com>
2016-08-30 10:36:23 -0400

    config bootstrap and babel and build

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var stormpath = require('express-stormpath');
 var webpack = require('webpack');
 var config = require('./webpack.config');
+var path = require('path');
 
 var app = express();
 
@@ -17,6 +18,14 @@ app.use(stormpath.init(app, {
         produces: ['application/json']
     }
 }));
+
+app.get('/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/css/bootstrap.min.css'));
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 app.on('stormpath.ready', function () {
     app.listen(8000, 'localhost', function (err) {
